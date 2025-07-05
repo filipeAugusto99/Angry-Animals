4a726aa070d4ad5bd13883aff097f82e530420a1
Filipe Augusto <filipecordeiro213@gmail.com>
2025-07-05 10:57:24 -0300

    Animal Scene

---
+++ Scene/Animal/Animal.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	pass # Replace with function body.
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	pass # Replace with function body.
