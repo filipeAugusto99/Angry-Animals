933b49731ebada2925dcbfe5cb694eb6b53f2644
Filipe Augusto <filipecordeiro213@gmail.com>
2025-07-12 15:26:30 -0300

    Water

--- Scene/LevelBase/LevelBase.gd
+++ Scene/LevelBase/LevelBase.gd
@@ -4,7 +4,6 @@ const ANIMAL = preload("res://Scene/Animal/Animal.tscn")
 
 @onready var animal_start: Marker2D = $AnimalStart
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_animal()
 	
@@ -15,3 +14,6 @@ func spawn_animal() -> void:
 	var animal = ANIMAL.instantiate()
 	animal.position = animal_start.position
 	add_child(animal)
+
+
+	
